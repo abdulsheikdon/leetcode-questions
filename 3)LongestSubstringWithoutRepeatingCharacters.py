25507d74092bb66234997375516cb3a2fca268ec
Abdullahi Sheikdon <126375017+abdulsheikdon@users.noreply.github.com>
2025-09-06 23:45:58 -0500

    Create 3)LongestSubstringWithoutRepeatingCharacters.py

---
+++ 3)LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        charSet = set()
+        left = 0
+        substringLength = 0
+
+        for right in range(len(s)):
+            while s[right] in charSet:
+                charSet.remove(s[left])
+                left += 1
+            charSet.add(s[right])
+            substringLength = max(substringLength, right - left + 1)
+
+        return substringLength
+
+        
